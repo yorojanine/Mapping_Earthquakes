53a3a193d46546501e29d96acffee9fca7b19c81
yorojanine <j9yoro@gmail.com>
2021-03-09 23:33:56 -0800

    markers

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,8 +2,9 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-//let map = L.map('mapid').setView([40.7, -94.5], 4);
-let map = L.map('mapid').setView([34.0522, -118.2437],14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+//los angeles
+//let map = L.map('mapid').setView([34.0522, -118.2437],14);
 
 // Create the map object with a center and zoom level. **ALTERNATE ** useful when we need to add multiple tile layers, or a background image of our map(s)
 // let map = L.map("mapid", {
@@ -34,15 +35,73 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
+// An array containing each city's location, state, and population.
+// let cities = [{
+//     location: [40.7128, -74.0059],
+//     city: "New York City",
+//     state: "NY",
+//     population: 8398748
+//   },
+//   {
+//     location: [41.8781, -87.6298],
+//     city: "Chicago",
+//     state: "IL",
+//     population: 2705994
+//   },
+//   {
+//     location: [29.7604, -95.3698],
+//     city: "Houston",
+//     state: "TX",
+//     population: 2325502
+//   },
+//   {
+//     location: [34.0522, -118.2437],
+//     city: "Los Angeles",
+//     state: "CA",
+//     population: 3990456
+//   },
+//   {
+//     location: [33.4484, -112.0740],
+//     city: "Phoenix",
+//     state: "AZ",
+//     population: 1660272
+//   }
+//   ];
+
+//get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+// cities.forEach(function(city) {
+//     console.log(city)
+//     L.marker(city.location).addTo(map);
+// });
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: "orange",
+        fillColor: '#ffffa1',
+        weight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+
+
+
 //** Add a circle to the map
 // L.circle([34.0522, -118.2437], {
 //     radius: 100
 //  }).addTo(map);
 
 //circlemarker
-L.circleMarker([34.0522, -118.2437],{
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
-})
-.addTo(map);
+// L.circleMarker([34.0522, -118.2437],{
+//     radius: 300,
+//     color: "black",
+//     fillColor: '#ffffa1'
+// })
+// .addTo(map);
+
