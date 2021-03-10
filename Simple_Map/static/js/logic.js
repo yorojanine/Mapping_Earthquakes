a6eaff97ebd7c2fd6f849fa4f6a13971f57c4386
yorojanine <j9yoro@gmail.com>
2021-03-10 00:12:50 -0800

    lines

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,93 +2,114 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+//let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
 //los angeles
 //let map = L.map('mapid').setView([34.0522, -118.2437],14);
 
-// Create the map object with a center and zoom level. **ALTERNATE ** useful when we need to add multiple tile layers, or a background image of our map(s)
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
+
+// // Create the map object with a center and zoom level. **ALTERNATE ** useful when we need to add multiple tile layers, or a background image of our map(s)
+// // let map = L.map("mapid", {
+// //     center: [
+// //       40.7, -94.5
+// //     ],
+// //     zoom: 4
+// //   });
+
+
+
+
 
 // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
-
-//blackbackground
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
+//blackbackground
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
 
 // Then we add our 'graymap' tile layer to the map.
  streets.addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// //  Add a marker to the map for Los Angeles, California.
+// // let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-// An array containing each city's location, state, and population.
-// let cities = [{
-//     location: [40.7128, -74.0059],
-//     city: "New York City",
-//     state: "NY",
-//     population: 8398748
-//   },
-//   {
-//     location: [41.8781, -87.6298],
-//     city: "Chicago",
-//     state: "IL",
-//     population: 2705994
-//   },
-//   {
-//     location: [29.7604, -95.3698],
-//     city: "Houston",
-//     state: "TX",
-//     population: 2325502
-//   },
-//   {
-//     location: [34.0522, -118.2437],
-//     city: "Los Angeles",
-//     state: "CA",
-//     population: 3990456
-//   },
-//   {
-//     location: [33.4484, -112.0740],
-//     city: "Phoenix",
-//     state: "AZ",
-//     population: 1660272
-//   }
-//   ];
+// // An array containing each city's location, state, and population.
+// // let cities = [{
+// //     location: [40.7128, -74.0059],
+// //     city: "New York City",
+// //     state: "NY",
+// //     population: 8398748
+// //   },
+// //   {
+// //     location: [41.8781, -87.6298],
+// //     city: "Chicago",
+// //     state: "IL",
+// //     population: 2705994
+// //   },
+// //   {
+// //     location: [29.7604, -95.3698],
+// //     city: "Houston",
+// //     state: "TX",
+// //     population: 2325502
+// //   },
+// //   {
+// //     location: [34.0522, -118.2437],
+// //     city: "Los Angeles",
+// //     state: "CA",
+// //     population: 3990456
+// //   },
+// //   {
+// //     location: [33.4484, -112.0740],
+// //     city: "Phoenix",
+// //     state: "AZ",
+// //     population: 1660272
+// //   }
+// //   ];
 
-//get data from cities.js
-let cityData = cities;
+// //get data from cities.js
+// let cityData = cities;
 
-// Loop through the cities array and create one marker for each city.
-// cities.forEach(function(city) {
+// // Loop through the cities array and create one marker for each city.
+// // cities.forEach(function(city) {
+// //     console.log(city)
+// //     L.marker(city.location).addTo(map);
+// // });
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
 //     console.log(city)
-//     L.marker(city.location).addTo(map);
+//     L.circleMarker(city.location, {
+//         radius: city.population/200000,
+//         color: "orange",
+//         fillColor: '#ffffa1',
+//         weight: 4
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
 // });
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/200000,
-        color: "orange",
-        fillColor: '#ffffa1',
-        weight: 4
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
 
 
 
